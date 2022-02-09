f47e17416448125e6a64bb7c4c6d73e0ebc63555
Saul Ojeda <saulojeda@ymail.com>
2022-02-09 11:18:38 -0800

    end of section 10

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -5,6 +5,7 @@ import { ToastrModule } from 'ngx-toastr';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { FileUploadModule } from 'ng2-file-upload';
 
 @NgModule({
   declarations: [],
@@ -15,6 +16,7 @@ import { NgxSpinnerModule } from 'ngx-spinner';
     TabsModule.forRoot(),
     NgxGalleryModule,
     NgxSpinnerModule,
+    FileUploadModule,
   ],
   exports: [
     BsDropdownModule,
@@ -22,6 +24,7 @@ import { NgxSpinnerModule } from 'ngx-spinner';
     TabsModule,
     NgxGalleryModule,
     NgxSpinnerModule,
+    FileUploadModule,
   ],
 })
 export class SharedModule {}
---
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -0,0 +1,85 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+import { take } from 'rxjs';
+import { Member } from 'src/app/models/member';
+import { Photo } from 'src/app/models/photo';
+import { User } from 'src/app/models/user';
+import { AccountService } from 'src/app/_services/account.service';
+import { MembersService } from 'src/app/_services/members.service';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-photo-editor',
+  templateUrl: './photo-editor.component.html',
+  styleUrls: ['./photo-editor.component.css'],
+})
+export class PhotoEditorComponent implements OnInit {
+  @Input() member: Member;
+  uploader: FileUploader;
+  hasBaseDropZoneOver = false;
+  baseUrl = environment.apiUrl;
+  user: User;
+
+  constructor(
+    private accountService: AccountService,
+    private memberService: MembersService
+  ) {
+    this.accountService.currentUser$
+      .pipe(take(1))
+      .subscribe((user) => (this.user = user));
+  }
+
+  ngOnInit(): void {
+    this.initializeUploader();
+  }
+
+  setMainPhoto(photo: Photo) {
+    this.memberService.setMainPhoto(photo.id).subscribe(() => {
+      this.user.photoUrl = photo.url;
+      // will update code user observable and photo inside local storage
+      this.accountService.setCurrentUser(this.user);
+      this.member.photoUrl = photo.url;
+      this.member.photos.forEach((p) => {
+        if (p.isMain) p.isMain = false;
+        if (p.id == photo.id) p.isMain = true;
+      });
+    });
+  }
+
+  deletePhoto(photoId: number) {
+    this.memberService.deletePhoto(photoId).subscribe(() => {
+      this.member.photos = this.member.photos.filter((x) => x.id != photoId);
+    });
+  }
+
+  fileOverBase(event: any) {
+    this.hasBaseDropZoneOver = event;
+  }
+
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + 'users/add-photo',
+      authToken: 'Bearer ' + this.user.token,
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024,
+    });
+
+    // for API CORS configuration
+    // this.uploader.onAfterAddingAll = (file) => {
+    //   file.withCredentials = false;
+    // };
+    this.uploader.onBeforeUploadItem = (file) => {
+      file.withCredentials = false;
+    };
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if (response) {
+        const photo = JSON.parse(response);
+        this.member.photos.push(photo);
+      }
+    };
+  }
+}
